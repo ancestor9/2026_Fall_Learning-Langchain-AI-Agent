481631524fba497f9f6943d7be20c873603b5a07
ancestor9 <46776608+ancestor9@users.noreply.github.com>
2026-08-19 18:04:41 +0900

    Delete scripts directory

--- scripts/prefect_sample/Code3.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def check_po():
-    po_df = pd.read_excel("POdb.xlsx")
-    po_df.to_csv("temp_po.csv", index=False)
-    print("[Code3] PO database checked successfully.")
-
-if __name__ == "__main__":
-    check_po()
